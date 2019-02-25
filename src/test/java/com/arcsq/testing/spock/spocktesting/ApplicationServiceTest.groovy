b90ce717b187f61dd9782f544ec7e1d9c974007a
Sunil Joshi <sunilsj@gmail.com>
2019-02-24 21:45:43 -0500

    Updates to testcases

--- src/test/java/com/arcsq/testing/spock/spocktesting/ApplicationServiceTest.groovy
+++ src/test/java/com/arcsq/testing/spock/spocktesting/ApplicationServiceTest.groovy
@@ -1,9 +1,17 @@
 package com.arcsq.testing.spock.spocktesting
 
+import groovy.sql.Sql
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class ApplicationServiceTest extends Specification {
+
+    @Shared sql = Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
+
+    def setupSpec() {
+        sql.execute("create table test (id int primary key)");
+    }
 
     @Unroll
     def "test if parameter #id and #result"() {
